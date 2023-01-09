43ba014c5cd4c547994e87742c7c273cb37a6283
GabrielPantoja96 <81129980+GabrielPB96@users.noreply.github.com>
2023-01-08 23:02:20 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import TaskList from "./components/container/task-list";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TaskList></TaskList>
+    </div>
+  );
+}
